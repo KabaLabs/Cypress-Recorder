f2fadb22339f9f0876297cf98edfadf1e857cf7e
brad morgan <bkmorgan3@gmail.com>
2019-10-11 18:34:52 -0700

    garbmerg

--- src/event-recording/EventRecorder.ts
+++ src/event-recording/EventRecorder.ts
@@ -1,1 +1,9 @@
 // event recorder
+function sum(a, b){
+    return a + b;
+}
+
+module.exports = sum;
+export default class EventRecorder {
+    
+}
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,1 +1,24 @@
 // configure webpack
+module.exports = {
+    module: {
+        loaders: [
+            { exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/ },
+            { loader: 'style-loader!css-loader', test: /\.css$/ },
+            { loader: 'url-loader', test: /\.gif$/ },
+            { loader: 'file-loader', test: /\.(ttf|eot|svg)$/ },
+        ],
+    },
+    resolve: {
+        alias: {
+            config$: './configs/app-config.js',
+            react: './vendor/react-master',
+        },
+        extensions: ['', 'js', 'jsx'],
+        modules: [
+            'node_modules',
+            'bower_components',
+            'shared',
+            '/shared/vendor/modules',
+        ],
+    },
+};
