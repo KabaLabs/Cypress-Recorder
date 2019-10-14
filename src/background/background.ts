edf728ac762d4b8eef74e422817e26ec391e5d87
Adam Stover <nehtaro@gmail.com>
2019-10-14 12:27:38 -0700

    modified background.ts to include comments. Mild refactor for stopRecording. cleanUp is now invoked on initialization.

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,3 +1,11 @@
+/**
+ * The background script which is always runnin'.
+ * 
+ * Serves as the router and controller for the extension; the popup sends messages to the background,
+ * and the background sets up the event recording and code generation and serves the resulting code
+ * back to the popup for display to the user.
+ */
+
 import { generateCode } from '../helpers/codeGenerator';
 import { RecAction, RecordedSession, ParsedEvent } from '../types/types';
 
@@ -7,40 +15,88 @@ const session: RecordedSession = {
   events: [],
 };
 
+/**
+ * Handles events sent from the event recorder.
+ * 
+ * @see handleNewConnection
+ * 
+ * @param event 
+ */
 function handleEvents(event: ParsedEvent): void {
-  console.dir(event);
   session.events.push(event);
 }
 
+
+/**
+ * Handles any new connections from event recorders.
+ * 
+ * Event recorders will open new connections upon injection into their tab;
+ * upon establishing this connection, we need to listen to any new messages on this port;
+ * this is how the event recorder sends the background information.
+ * 
+ * @see handleEvents
+ * @see initialize
+ * 
+ * @param portToEventRecorder 
+ */
 function handleNewConnection(portToEventRecorder: chrome.runtime.Port): void {
   port = portToEventRecorder;
   session.sender = port.sender;
   port.onMessage.addListener(handleEvents);
 }
 
+/**
+ * Starts the recording process by injecting the event recorder into the active tab.
+ * 
+ * @see handleControlAction
+ */
 function startRecording(): void {
   chrome.tabs.executeScript({ file: '/content-scripts/eventRecorder.js', allFrames: true });
 }
 
-function stopRecording(): void {
+/**
+ * Stops the recording process.
+ * 
+ * @see handleControlAction
+ * 
+ * @param sendResponse 
+ */
+function stopRecording(sendResponse: (response: any) => void): void {
   port.postMessage({ type: 'stopRec' });
+  const code = generateCode(session);
+  sendResponse(code);
+  chrome.storage.local.set({ codeBlocks: code });
 }
 
+/**
+ * Resets the recording process by disconnecting from the event recorder and clearing the events store.
+ * 
+ * @see handleControlAction
+ */
 function resetRecording(): void {
   port.disconnect();
   session.events = [];
 }
 
+/**
+ * Handles control messages sent from the view (popup) and conducting the appropriate actions.
+ * 
+ * @see initialize
+ * @see startRecording
+ * @see stopRecording
+ * @see resetRecording
+ * 
+ * @param action 
+ * @param sender 
+ * @param sendResponse 
+ */
 function handleControlAction(action: RecAction, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void): void {
   switch (action.type) {
     case 'startRec':
       startRecording();
       break;
     case 'stopRec':
-      stopRecording();
-      const code = generateCode(session);
-      sendResponse(code);
-      chrome.storage.local.set({ codeBlocks: code });
+      stopRecording(sendResponse);
       break;
     case 'resetRec':
       resetRecording();
@@ -50,12 +106,24 @@ function handleControlAction(action: RecAction, sender: chrome.runtime.MessageSe
   }
 }
 
+/**
+ * Performs necessary cleanup on startup and suspend.
+ * 
+ * @see initialize
+ */
 function cleanUp(): void {
   chrome.storage.local.set({ status: 'off' });
 }
 
+/**
+ * Initializes the extension.
+ * 
+ * @see cleanUp
+ * @see handleControlAction
+ * @see handleNewConnection
+ */
 function initialize(): void {
-  chrome.runtime.onStartup.addListener(cleanUp);
+  cleanUp();
   chrome.runtime.onMessage.addListener(handleControlAction);
   chrome.runtime.onConnect.addListener(handleNewConnection);
   chrome.runtime.onSuspend.addListener(cleanUp);
