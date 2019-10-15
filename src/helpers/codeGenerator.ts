6135559d0233800ca46261807f0a11a575f8228a
abbeycampbell <campbellabbeya@gmail.com>
2019-10-15 16:56:33 -0700

    added code generator comments, MVP code generator functionality

--- src/helpers/codeGenerator.ts
+++ src/helpers/codeGenerator.ts
@@ -1,30 +1,18 @@
 /**
- * Code Generator is the function that actually generates the Cypress code that will simulate 
- * the recorded user session.
- * 
- * Each time the user records, this function will generate:
- * - a 'describe' function block with a placeholder for the user to title their test. It contains:
- * - an 'it' function block with a placeholder for a description of the interactions taking place. It contains:
- * - a cy.visit command for the current url
- * - each subsequest user interaction with the browser, e.g. (cy.get('element').click)
+ * Actually generates the Cypress code that will simulate the recorded user session.
+ *
+ * Each time the user records, this function will generate a cy.visit command that will
+ * store the current url, as well each subsequest user interaction with the browser.
  */
 
- /**
-  * SAMPLE CODE:
-  * cy.get('.action-email')
-    .type('fake@email.com').should('have.value', 'fake@email.com')
-    
-    cy.get('.action-btn').click() <= you can pass coordinates in
-
-    cy.get('.action-form')
-    .find('[type="text"]').type('HALFOFF')
-    cy.get('.action-form').submit()
-    .next().should('contain', 'Your form has been submitted!')
-  */
 import { RecordedSession, ParsedEvent, BlockData, CodeBlock } from '../types';
 import { EventType } from '../constants';
 
-// write helper functions to handle each action type
+/**
+ * Helper fucntions that handle each action type.
+ * @param event 
+ */
+
 function handleClick(event: ParsedEvent): CodeBlock {
   return `cy.get('${event.selector}').click()`;
 }
@@ -77,10 +65,13 @@ function handleSubmit(event: ParsedEvent): CodeBlock {
   return 'submit';
 }
 
+/**
+ * Generates a line of Cypress code that replicates an action by a user.
+ * @param event 
+ */
+
 function generateBlock(event: ParsedEvent): CodeBlock {
-  // place in correct area
   console.log('event', event);
-  // takes events object and translates it to block(s) of code (as a string)
   switch (event.action) {
     case EventType.CLICK:
       return handleClick(event);
@@ -97,8 +88,12 @@ function generateBlock(event: ParsedEvent): CodeBlock {
     default:
       throw new Error(`Unhandled event: ${event.action}`);
   }
-  // return event.selector; // is this codeblock instead? returning string
 }
+
+/**
+ * Exports array of all Cypress commands.
+ * @param session 
+ */
 
 export default function generateCode(session: RecordedSession): BlockData {
   return [`cy.visit('${session.sender.url}');` as CodeBlock]
