15f5185cc3612dd494631bb040bd21e47c0ab238
abbeycampbell <campbellabbeya@gmail.com>
2019-10-17 00:15:33 -0700

    added cypress cone svg as react component

--- src/popup/components/ActiveRecordingBox.tsx
+++ src/popup/components/ActiveRecordingBox.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import CypressCone from './CypressCone';
 
 export interface ActiveRecordingBoxProps {
 
@@ -6,6 +7,7 @@ export interface ActiveRecordingBoxProps {
 
 export default (props: ActiveRecordingBoxProps) => (
   <div id="active-box">
+    <CypressCone />
     <p className="text">Click 'Stop Recording' to see the results of your session!</p>
   </div>
 );
