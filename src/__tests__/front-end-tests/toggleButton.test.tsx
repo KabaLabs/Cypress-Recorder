99070d7e291d275402e617bbdb0780caf72847ac
abbeycampbell <campbellabbeya@gmail.com>
2019-10-21 12:33:32 -0700

    updated test to reflect invalid tab case

--- src/__tests__/front-end-tests/toggleButton.test.tsx
+++ src/__tests__/front-end-tests/toggleButton.test.tsx
@@ -6,25 +6,33 @@ import "../../setupTests"
 describe('toggle button', () => {
   let wrapper;
   const handleToggleMock = jest.fn()
-  it('should call handletoggle with argument "startRec" when clicked and recStatus is "off"', () => {
-    wrapper = shallow(<ToggleButton recStatus='off' handleToggle={handleToggleMock}/>)
+  it('should not call handletoggle when clicked and recStatus is "off" and tab is invalid', () => {
+    wrapper = shallow(<ToggleButton recStatus='off' isValidTab={false} handleToggle={handleToggleMock}/>)
     expect(wrapper).toMatchSnapshot();
     const button = wrapper.find('button');
     button.simulate('click');
-    expect(handleToggleMock).toHaveBeenCalledWith('startRec')
+    expect(handleToggleMock).not.toHaveBeenCalled();
+    expect(button.disabled).toBe(true);
+  })
+  it('should call handletoggle with argument "startRec" when clicked and recStatus is "off" and tab is valid', () => {
+    wrapper = shallow(<ToggleButton recStatus='off' isValidTab={true} handleToggle={handleToggleMock}/>)
+    expect(wrapper).toMatchSnapshot();
+    const button = wrapper.find('button');
+    button.simulate('click');
+    expect(handleToggleMock).toHaveBeenCalledWith('startRec');
   })
   it('should call handletoggle with argument "stopRec" when clicked and recStatus is "on"', () => {
-    wrapper = shallow(<ToggleButton recStatus='on' handleToggle={handleToggleMock}/>)
+    wrapper = shallow(<ToggleButton recStatus='on' isValidTab={true} handleToggle={handleToggleMock}/>)
     expect(wrapper).toMatchSnapshot();
     const button = wrapper.find('button');
     button.simulate('click');
     expect(handleToggleMock).toHaveBeenCalledWith('stopRec');
   })
   it('should call handletoggle with argument "resetRec" when clicked and recStatus is "done"', () => {
-    wrapper = shallow(<ToggleButton recStatus='done' handleToggle={handleToggleMock}/>)
+    wrapper = shallow(<ToggleButton recStatus='done' isValidTab={true} handleToggle={handleToggleMock}/>)
     expect(wrapper).toMatchSnapshot();
     const button = wrapper.find('button');
     button.simulate('click');
-    expect(handleToggleMock).toHaveBeenCalledWith('resetRec')
+    expect(handleToggleMock).toHaveBeenCalledWith('resetRec');
   })
 })
