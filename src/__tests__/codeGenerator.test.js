3acf091312b4bf27a4d4e64149d0f530ce45d480
abbeycampbell <campbellabbeya@gmail.com>
2019-10-20 17:58:35 -0700

    set up jest/enzyme

---
+++ src/__tests__/codeGenerator.test.js
@@ -0,0 +1,7 @@
+// unit tests for code generator
+// test that is irrelevent
+describe('title', () => {
+  it('has an h1 tag with the text cypress recorder', () => {
+    expect(true).toBe(true);
+  })
+})
