5452ef46d73b8c55597166879979c6fe72d0b64f
Josh Soref <jsoref@users.noreply.github.com>
2021-06-20 13:25:43 -0400

    spelling: occurring
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/background/background.ts
+++ src/background/background.ts
@@ -43,7 +43,7 @@ function control(cb: (...args: any) => Promise<void>, cmd?: string | ActionWithP
  * Injects the event recorder into the active tab.
  *
  * @param details If the details argument is present, that means that web navigation occurred, and
- * we want to ensure that this navigation is occuring in the top-level frame.
+ * we want to ensure that this navigation is occurring in the top-level frame.
  */
 function injectEventRecorder(
   details?: chrome.webNavigation.WebNavigationFramedCallbackDetails,
